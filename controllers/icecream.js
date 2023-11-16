c4c15a2a3bc0ad5f9f0592983fe1b4562eb53dcd
kavyasree05 <137372024+kavyasree05@users.noreply.github.com>
2023-11-15 22:12:43 -0600

    Updated icecreamdetail and icecreamupdate

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -134,4 +134,18 @@ exports.icecream_update_Page = async function(req, res) {
     res.status(500)
     res.send(`{'error': '${err}'}`);
     }
-    }; 
+    };
+
+    // Handle a delete one view with id from query
+exports.icecream_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await icecream.findById(req.query.id)
+    res.render('icecreamdelete', { title: 'icecream Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
